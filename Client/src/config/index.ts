27f1e0fa274071d9313136069f330f12dfe7758a
Pablo Risi <pablomrisi@hotmail.com>
2026-07-16 19:16:56 -0300

    railway configuracion

--- Client/src/config/index.ts
+++ Client/src/config/index.ts
@@ -1,7 +1,9 @@
 export const config = {
     Api: import.meta.env.VITE_API_URL || 
-         (import.meta.env.DEV ? 'http://localhost:3001/api' : 'https://ecommers-petshop.onrender.com/api')
+         (import.meta.env.DEV ? 'http://localhost:3001/api' : 'https://ecommers-petshop-production.up.railway.app/api')
 };
 
 
 
+
+// https://ecommers-petshop.onrender.com/api
--- Server/config/cors.js
+++ Server/config/cors.js
@@ -5,6 +5,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5174',
     'https://ecommers-petshop.vercel.app',
+    'https://ecommers-petshop-production.up.railway.app',
     'https://api.bambinapetshop.com',
     'https://bambinapetshop.com', 
     'https://www.bambinapetshop.com'
